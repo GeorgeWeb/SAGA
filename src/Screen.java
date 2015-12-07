c1b9a7d02c8c283bbffd0aa45c34f9c50bea25ca
GeorgiBG <40208393@live.napier.ac.uk>
2015-12-07 11:11:08 +0000

    LAST COMMIT FOR THE MORNING

--- src/Screen.java
+++ src/Screen.java
@@ -3,15 +3,9 @@ public class Screen
 {
 	public static void main(String[] args) 
 	{
-<<<<<<< HEAD
-<<<<<<< HEAD
 		System.out.print("Test/Hello World! Dicks\n Pishki\n");
-=======
 		System.out.print("Test/Hello World! Dicks\nSreden prust WE\n");
->>>>>>> origin/master
-=======
 		System.out.print("Test/Hello World! Dicks\nSreden prust WE\n/QJ MI KURA ANDREW");
->>>>>>> origin/master
 	}
 
 }
