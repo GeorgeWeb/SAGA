ecf57cf7ff04ee969c09f2d1d5ee85a81be9d7f5
Georgi Mirazchiyski <ivorbk@abv.bg>
2015-12-09 22:37:58 +0200

    asd

--- andrEW/TestTomcat/src/Testing.java
+++ andrEW/TestTomcat/src/Testing.java
@@ -26,34 +26,55 @@ public class Testing extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
-    static void drawFrame(Graphics2D g, int frame){
+    static void drawFrame(Graphics2D g, int frame)
+    {
     	  g.setColor(Color.green);
     	  g.fillRect(0,0,150,100);
     	  g.setColor(Color.red);
     	  g.fillRect(50,8*frame-20,20,50);
+	}
+    
+    public static int xpos = 500;
+    
+    static void animateString()
+    {
+    	Graphics2D g2;
+    	g2.drawString("ZDR Andrew :)", xpos, 30);
+    	xpos--;
+    	if(xpos < -200)
+    	{
+    		xpos = 500;
     	}
+    }
     
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException 
+	{
 		// TODO Auto-generated method stub
     	res.setContentType("image/gif");
-	    
+    	
     	try 
     	{    		
     		AnimatedGifEncoder e = new AnimatedGifEncoder();
     		
     		e.start(res.getOutputStream());
     		e.setDelay(300);   // 1 frame per sec
-            for (int i=0;i<20;i++){
-              BufferedImage image = new BufferedImage(150,100, BufferedImage.TYPE_INT_ARGB);              
-              Graphics2D g = image.createGraphics();
-              drawFrame(g,i);
-              e.addFrame(image);
-		      //e.addFrame(image2); 
-              g.dispose();
+            
+    		for (int i=0;i<20;i++)
+            {
+            	BufferedImage image = new BufferedImage(150,100, BufferedImage.TYPE_INT_ARGB);
+            	BufferedImage image2 = new BufferedImage(650,100, BufferedImage.TYPE_INT_ARGB);
+            	Graphics2D g = image.createGraphics();
+            	drawFrame(g,i);
+            	animateString();
+            	e.addFrame(image);
+            	e.addFrame(image2);
+            	//e.addFrame(image2); 
+            	g.dispose();
             }
+            
             e.finish();
         }
     	catch(Exception e)
@@ -69,5 +90,4 @@ public class Testing extends HttpServlet {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
-
 }
