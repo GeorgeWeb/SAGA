d286f1b313bfd216d5de375f78550aa500781abb
Georgi Mirazchiyski <ivorbk@abv.bg>
2015-12-10 23:18:23 +0000

    RABOTI

--- andrEW/TestTomcat/src/Testing.java
+++ andrEW/TestTomcat/src/Testing.java
@@ -1,5 +1,4 @@
 
-
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
@@ -20,7 +19,8 @@ import javax.servlet.http.HttpServletResponse;
  * Servlet implementation class Testing
  */
 @WebServlet("/Testing")
-public class Testing extends HttpServlet {
+public class Testing extends HttpServlet 
+{
 	private static final long serialVersionUID = 1L;
        
     /**
@@ -30,8 +30,8 @@ public class Testing extends HttpServlet {
         super();
         // TODO Auto-generated constructor stub
     }
-    
-    final int carY = 210;
+    final int carXOffset = 135;
+    final int carYOffset = 210;
     
     public void drawRoad(Graphics2D g)
     {
@@ -39,14 +39,73 @@ public class Testing extends HttpServlet {
         g.fillRect(0, 240, 500, 30);
     }
     
+    public void drawClouds(Graphics2D g)
+    {
+    	//Cloud1
+    	g.setColor(Color.WHITE);
+    	g.fillRect(18, 25 + 20, 30, 10);
+    	g.fillRect(40, 15 + 20, 40, 20);
+    	g.fillRect(72, 22 + 20, 30, 13);
+        g.fillRect(10, 35 + 20, 100, 10);
+        
+        //Cloud2
+        g.setColor(Color.WHITE);
+    	g.fillRect(18 + 120, 25 + 50, 30, 10);
+    	g.fillRect(40 + 120, 15 + 50, 40, 20);
+    	g.fillRect(72 + 120, 22 + 50, 30, 13);
+        g.fillRect(10 + 120, 35 + 50, 100, 10);
+        
+        //Cloud3
+        g.setColor(Color.WHITE);
+    	g.fillRect(18 + 240, 25, 30, 10);
+    	g.fillRect(40 + 240, 15, 40, 20);
+    	g.fillRect(72 + 240, 22, 30, 13);
+        g.fillRect(10 + 240, 35, 100, 10);
+        
+        //Cloud4
+        g.setColor(Color.WHITE);
+    	g.fillRect(18 + 360, 25 + 70, 30, 10);
+    	g.fillRect(40 + 360, 15 + 70, 40, 20);
+    	g.fillRect(72 + 360, 22 + 70, 30, 13);
+        g.fillRect(10 + 360, 35 + 70, 100, 10);
+    }
+    
+    public void drawBlockType1(Graphics2D g)
+    {
+    	g.setColor(Color.DARK_GRAY);
+        g.fillRect(30, 100, 120, 140);
+    }
+    
+    public void drawBlockType2(Graphics2D g)
+    {
+    	g.setColor(Color.DARK_GRAY);
+        g.fillRect(180, 80, 100, 160);
+    }
+    
+    public void drawBlockType3(Graphics2D g)
+    {
+    	g.setColor(Color.DARK_GRAY);
+        g.fillRect(310, 130, 140, 110);
+    }
+    
     public void drawTree(Graphics2D g)
     {
     	// tree top
-        g.setColor(Color.GREEN);
-        g.fillRect(0, 160, 30, 20);
+        g.setColor(new Color(0, 153, 76));
+        g.fillRect(185, 160, 40, 40);
         // tree body
         g.setColor(new Color(139, 69, 19));
-        g.fillRect(0, 200, 10, 40);
+        g.fillRect(200, 200, 10, 40);
+    }
+    
+    public void drawTree2(Graphics2D g)
+    {
+    	// tree top
+        g.setColor(new Color(0, 153, 76));
+        g.fillRect(185 + 100, 160, 40, 40);
+        // tree body
+        g.setColor(new Color(139, 69, 19));
+        g.fillRect(200, 200, 10, 40);
     }
     
     public void drawBackground(Graphics2D g)
@@ -55,21 +114,26 @@ public class Testing extends HttpServlet {
         g.fillRect(0, 0, 500, 270);
     }
     
-    public void drawCar(Graphics2D g, int frame) 
+    public void drawCar1(Graphics2D g, int frame) 
     {
         g.setColor(Color.YELLOW); // main body
-        g.fillRect(frame + 40, carY, 90, 25);
+        g.fillRect(frame + 40 - carXOffset, carYOffset, 90, 25);
         g.setColor(Color.GRAY); // top body
-        g.fillRect(frame + 60, carY - 20, 50, 20);
+        g.fillRect(frame + 60 - carXOffset, carYOffset - 20, 50, 20);
         g.setColor(Color.BLACK); // wheels
-        g.fillRect(frame + 55, carY + 17, 15, 15); // wheel
-        g.fillRect(frame + 100, carY + 17, 15, 15); // wheel
-        g.translate(frame + 250, frame + 250);
+        g.fillRect(frame + 55 - carXOffset, carYOffset + 17, 15, 15); // wheel
+        g.fillRect(frame + 100 - carXOffset, carYOffset + 17, 15, 15); // wheel
     }
     
-    public void moveRider(Graphics g, int frame)
+    public void drawCar2(Graphics2D g, int frame) 
     {
-    	g.translate(frame + 250, frame + 250);
+        g.setColor(Color.RED); // main body
+        g.fillRect(frame + 40 - carXOffset * 7, carYOffset, 90, 25);
+        g.setColor(Color.BLACK); // top body
+        g.fillRect(frame + 60 - carXOffset * 7, carYOffset - 20, 50, 20);
+        g.setColor(Color.BLACK); // wheels
+        g.fillRect(frame + 55 - carXOffset * 7, carYOffset + 17, 15, 15);
+        g.fillRect(frame + 100 - carXOffset * 7, carYOffset + 17, 15, 15);
     }
     
 	/**
@@ -81,34 +145,36 @@ public class Testing extends HttpServlet {
     	res.setContentType("image/gif");
     	
     	try
-    	{    		
+    	{
     		AnimatedGifEncoder e = new AnimatedGifEncoder();
     		
     		e.start(res.getOutputStream());
-    		e.setDelay(10);   // frames per sec
-            
-    		
+    		//e.setDelay(1000);   // frames per sec
 
-			// create RIDER
-    		URL url = new URL("http://i.share.pho.to/72dbe770_o.png");
-    		Image img = ImageIO.read(url);
-    		
-    		for(int i = 0; i<300; i++)
+    		for(int i = 0; i < 150; i++)
     		{    			
     			BufferedImage image = new BufferedImage(500,270, BufferedImage.TYPE_INT_ARGB);
-    			//BufferedImage rider = (BufferedImage) img;
     			
             	Graphics2D g = image.createGraphics();
-            	//Graphics g2 = rider.getGraphics();
             	
             	drawBackground(g);
             	drawRoad(g);
-            	drawCar(g, i);
-            	//moveRider(g2, i);
+            	drawClouds(g);
+            	
+            	drawBlockType1(g);
+            	drawBlockType2(g);
+            	drawBlockType3(g);
+            	
+            	drawTree(g);
+            	drawTree2(g);
+            	
+            	g.translate(i*3, 0);
+            	drawCar1(g, i);            	
+            	g.translate(i*10, 0);
+            	drawCar2(g, i);
             	
             	e.addFrame(image);
-            	//e.addFrame(rider);
-            	
+
             	g.dispose();
     		}
     		
